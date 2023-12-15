e90dd30557ee1530c74203830eed3dff04b9e0cd
Maerkova <Maerkova@MAERYK>
2023-12-16 00:50:39 +0700

    Добавьте файлы проекта.

---
+++ Android3/Android3.NativeActivity/main.cpp
@@ -0,0 +1,62 @@
+﻿#include <SDL.h>
+
+// Define your table properties
+#define TABLE_WIDTH 400
+#define TABLE_HEIGHT 600
+#define CELL_WIDTH 100
+#define CELL_HEIGHT 40
+
+// Function to update table logic
+void update_table_logic(SDL_Event* event) {
+	// Implement your table logic here
+	if (event->type == SDL_QUIT) {
+		exit(0); // Exit the application if the window is closed
+	}
+}
+
+// Function to draw the table
+void draw_table(SDL_Renderer* renderer) {
+	// Clear the renderer
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_RenderClear(renderer);
+
+	// Example: Draw a simple grid
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+	for (int x = 0; x <= TABLE_WIDTH; x += CELL_WIDTH) {
+		SDL_RenderDrawLine(renderer, x, 0, x, TABLE_HEIGHT);
+	}
+
+	for (int y = 0; y <= TABLE_HEIGHT; y += CELL_HEIGHT) {
+		SDL_RenderDrawLine(renderer, 0, y, TABLE_WIDTH, y);
+	}
+
+	// Implement your cell rendering here
+
+	// Present the renderer
+	SDL_RenderPresent(renderer);
+}
+
+void android_main(struct android_app* state) {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		// Handle initialization failure
+		return;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Table Editor", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, TABLE_WIDTH, TABLE_HEIGHT, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	while (1) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event) != 0) {
+			update_table_logic(&event);
+		}
+
+		update_table_logic(&event);
+		draw_table(renderer);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
